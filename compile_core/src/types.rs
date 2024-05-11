dd4f4b69ae38a18a0ed4ab705f94cb7dfb49167b
Ryan Sadler <rrsadler@gmail.com>
2024-05-10 21:13:39 -0700

    update

--- compile_core/src/types.rs
+++ compile_core/src/types.rs
@@ -2,6 +2,7 @@ use crate::{InternKey, InternPool, InternValue, StringKey};
 use anyhow::Result;
 use ena::unify::*;
 use serde::Serialize;
+use std::convert::Into;
 use thiserror::Error;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
@@ -19,15 +20,6 @@ impl InternKey for TypeId {
 }
 
 pub type TypePool = InternPool<TypeId, AstType>;
-
-#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
-pub struct IntKey(u32);
-
-#[derive(Debug, Error)]
-pub enum UError {
-    #[error("Bad")]
-    Bad,
-}
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize)]
 pub enum AstType {
@@ -116,8 +108,40 @@ impl AstType {
     }
 }
 
+#[derive(Debug, Error)]
+pub enum UError {
+    #[error("Bad")]
+    Bad,
+}
+
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub struct IntKey(u32);
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize)]
+pub struct UType(AstType);
+
+impl std::ops::Deref for UType {
+    type Target = AstType;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<AstType> for UType {
+    fn from(item: AstType) -> Self {
+        Self(item)
+    }
+}
+
+impl Into<AstType> for UType {
+    fn into(self) -> AstType {
+        self.0
+    }
+}
+
 impl UnifyKey for IntKey {
-    type Value = AstType;
+    type Value = UType;
     fn index(&self) -> u32 {
         self.0
     }
@@ -129,9 +153,9 @@ impl UnifyKey for IntKey {
     }
     fn order_roots(
         a: IntKey,
-        a_rank: &AstType,
+        a_rank: &UType,
         b: IntKey,
-        b_rank: &AstType,
+        b_rank: &UType,
     ) -> Option<(IntKey, IntKey)> {
         if a_rank > b_rank {
             Some((a, b))
@@ -143,72 +167,76 @@ impl UnifyKey for IntKey {
     }
 }
 
-impl UnifyValue for AstType {
+fn ast_unify_values(value1: &AstType, value2: &AstType) -> Result<AstType, UError> {
+    let id1 = value1.try_unknown();
+    let id2 = value2.try_unknown();
+    match (id1, id2) {
+        (None, None) => match (value1, value2) {
+            //(Self::Int, Self::Float) | (Self::Float, Self::Int) => {
+            //Ok(Self::Number)
+            //}
+            (AstType::Ptr(v1), AstType::Ptr(v2)) => {
+                let ty = ast_unify_values(v1, v2)?;
+                Ok(AstType::Ptr(ty.into()))
+            }
+            (AstType::Func(c1, r1), AstType::Func(c2, r2)) => {
+                let r = ast_unify_values(r1, r2)?;
+                if c1.len() != c2.len() {
+                    Err(UError::Bad)
+                } else {
+                    let result = c1
+                        .iter()
+                        .zip(c2.iter())
+                        .map_while(|(a, b)| match ast_unify_values(a, b) {
+                            Ok(s) => Some(s),
+                            Err(_) => None,
+                        })
+                        .collect::<Vec<_>>();
+                    if result.len() == c1.len() {
+                        Ok(AstType::Func(result, r.into()))
+                    } else {
+                        Err(UError::Bad)
+                    }
+                }
+            }
+            (AstType::Tuple(c1), AstType::Tuple(c2)) => {
+                if c1.len() != c2.len() {
+                    Err(UError::Bad)
+                } else {
+                    let result = c1
+                        .iter()
+                        .zip(c2.iter())
+                        .map_while(|(a, b)| match ast_unify_values(a, b) {
+                            Ok(s) => Some(s),
+                            Err(_) => None,
+                        })
+                        .collect::<Vec<_>>();
+                    if result.len() == c1.len() {
+                        Ok(AstType::Tuple(result))
+                    } else {
+                        Err(UError::Bad)
+                    }
+                }
+            }
+            _ => {
+                if value1 == value2 {
+                    Ok(value1.clone())
+                } else {
+                    Err(UError::Bad)
+                }
+            }
+        },
+        (Some(_), None) => Ok(value2.clone()),
+        (None, Some(_)) => Ok(value1.clone()),
+        (Some(a), Some(_)) => Ok(AstType::Variable(a)),
+    }
+}
+
+impl UnifyValue for UType {
     type Error = UError;
 
     fn unify_values(value1: &Self, value2: &Self) -> Result<Self, UError> {
-        let id1 = value1.try_unknown();
-        let id2 = value2.try_unknown();
-        match (id1, id2) {
-            (None, None) => match (value1, value2) {
-                //(Self::Int, Self::Float) | (Self::Float, Self::Int) => {
-                //Ok(Self::Number)
-                //}
-                (Self::Ptr(v1), Self::Ptr(v2)) => {
-                    let ty = Self::unify_values(v1, v2)?;
-                    Ok(Self::Ptr(ty.into()))
-                }
-                (AstType::Func(c1, r1), AstType::Func(c2, r2)) => {
-                    let r = Self::unify_values(r1, r2)?;
-                    if c1.len() != c2.len() {
-                        Err(UError::Bad)
-                    } else {
-                        let result = c1
-                            .iter()
-                            .zip(c2.iter())
-                            .map_while(|(a, b)| match Self::unify_values(a, b) {
-                                Ok(s) => Some(s),
-                                Err(_) => None,
-                            })
-                            .collect::<Vec<_>>();
-                        if result.len() == c1.len() {
-                            Ok(AstType::Func(result, r.into()))
-                        } else {
-                            Err(UError::Bad)
-                        }
-                    }
-                }
-                (AstType::Tuple(c1), AstType::Tuple(c2)) => {
-                    if c1.len() != c2.len() {
-                        Err(UError::Bad)
-                    } else {
-                        let result = c1
-                            .iter()
-                            .zip(c2.iter())
-                            .map_while(|(a, b)| match Self::unify_values(a, b) {
-                                Ok(s) => Some(s),
-                                Err(_) => None,
-                            })
-                            .collect::<Vec<_>>();
-                        if result.len() == c1.len() {
-                            Ok(AstType::Tuple(result))
-                        } else {
-                            Err(UError::Bad)
-                        }
-                    }
-                }
-                _ => {
-                    if value1 == value2 {
-                        Ok(value1.clone())
-                    } else {
-                        Err(UError::Bad)
-                    }
-                }
-            },
-            (Some(_), None) => Ok(value2.clone()),
-            (None, Some(_)) => Ok(value1.clone()),
-            (Some(a), Some(_)) => Ok(Self::Variable(a)),
-        }
+        ast_unify_values(&value1.0, &value2.0).map(|ast| UType(ast))
     }
 }
 
@@ -229,15 +257,15 @@ impl TypeUnify {
         }
     }
 
-    pub fn fresh_unknown(&mut self) -> AstType {
+    pub fn fresh_unknown(&mut self) -> UType {
         let offset = self.variables.len();
-        let r = AstType::Variable(offset as u32);
+        let r = UType(AstType::Variable(offset as u32));
         self.variables.push(self.ut.new_key(r.clone()));
         r
     }
 
-    fn add_var(&mut self, a: AstType) -> IntKey {
-        match a {
+    fn add_var(&mut self, a: UType) -> IntKey {
+        match a.0 {
             AstType::Variable(v) => self.variables[v as usize],
             _ => self.ut.new_key(a),
         }
@@ -252,11 +280,11 @@ impl TypeUnify {
             }
             (AstType::Variable(v1), _) => {
                 let k1 = self.variables[*v1 as usize];
-                let k2 = self.ut.new_key(b.clone());
+                let k2 = self.ut.new_key(b.clone().into());
                 self.ut.unify_var_var(k1, k2)
             }
             (_, AstType::Variable(v2)) => {
-                let k1 = self.ut.new_key(a.clone());
+                let k1 = self.ut.new_key(a.clone().into());
                 let k2 = self.variables[*v2 as usize];
                 self.ut.unify_var_var(k1, k2)
             }
@@ -324,7 +352,7 @@ impl TypeUnify {
             AstType::Variable(offset) => {
                 let k = self.variables[*offset as usize];
                 let v = self.ut.probe_value(k);
-                if let AstType::Variable(_) = v {
+                if let AstType::Variable(_) = *v {
                     None
                 } else {
                     self.resolve(&v)
@@ -364,8 +392,8 @@ mod tests {
         let ut9 = u.fresh_unknown();
         let ut10 = u.fresh_unknown();
         u.unify(&ut9, &ut10).unwrap();
-        let ut11 = AstType::Tuple(vec![ut7, AstType::Int, ut8]);
-        let ut12 = AstType::Tuple(vec![AstType::Float, ut9.clone(), ut10.clone()]);
+        let ut11 = AstType::Tuple(vec![ut7.0, AstType::Int, ut8.0]);
+        let ut12 = AstType::Tuple(vec![AstType::Float, ut9.0.clone(), ut10.0.clone()]);
         let ut13 = u.fresh_unknown();
         u.unify(&ut11, &ut12).unwrap();
         u.unify(&ut12, &ut13).unwrap();
@@ -381,7 +409,7 @@ mod tests {
 
         // test ptr unify
         let ut14 = u.fresh_unknown();
-        let p0 = AstType::Ptr(ut14.clone().into());
+        let p0 = AstType::Ptr(Box::new(ut14.0.clone()));
         let p1 = AstType::Ptr(AstType::Int.into());
         u.unify(&p0, &p1).unwrap();
         println!("{:?}", u.resolve(&ut14));
@@ -391,10 +419,10 @@ mod tests {
         let ut15 = u.fresh_unknown();
         let f0 = AstType::Func(vec![AstType::Int], AstType::Int.into());
         let f1 = AstType::Func(vec![AstType::Float], AstType::Float.into());
-        let f2 = AstType::Func(vec![ut15.clone()], AstType::Int.into());
+        let f2 = AstType::Func(vec![ut15.0.clone()], AstType::Int.into());
         let ut16 = u.fresh_unknown();
         let ut17 = u.fresh_unknown();
-        let f3 = AstType::Func(vec![ut16.clone()], ut17.clone().into());
+        let f3 = AstType::Func(vec![ut16.0.clone()], Box::new(ut17.0.clone()));
         assert!(u.unify(&f0, &f2).is_ok());
         assert!(u.unify(&f1, &f2).is_err());
         assert!(u.unify(&f2, &f3).is_ok());
